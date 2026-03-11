6e26e271d63fd75e8263b38e4b8ff080d9d25b2d
D4R10W3STD1JK <dariowestdijk@protonmail.com>
2026-03-11 17:42:40 +0100

    Tee exercise finished

--- chapter04/tee.c
+++ chapter04/tee.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
 
 int main(int argc, char *argv[]) {
   int opt;
@@ -26,9 +29,40 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < numargs; i++) {
       files[i] = argv[optind + i];
     }
-    for (int i = 0; i < numargs; i++){
-      printf("%s\n", files[i]);
+  }
+  
+  char buf[BUF_SIZE];
+  ssize_t numread;
+
+  int outputFD[numargs], openflags;
+  mode_t filePerms;
+  
+  if (numa > 0) {
+    openflags = O_WRONLY | O_CREAT | O_APPEND;
+  }
+  else {
+    openflags = O_WRONLY | O_CREAT | O_TRUNC;
+  }
+  
+  for (int i = 0; i < numargs; i++) {
+    outputFD[i] = open(files[i], openflags, S_IRUSR | S_IWUSR);
+
+    if (outputFD[i] == -1)
+      exit(EXIT_FAILURE);
+  }
+
+  while ((numread = read(STDIN_FILENO, buf, BUF_SIZE)) > 0) {
+    if (write(STDOUT_FILENO, buf, numread) != numread)
+      exit(EXIT_FAILURE);
+
+    for(int i = 0; i < numargs; i++) {
+      if (write(outputFD[i], buf, numread) != numread)
+        exit(EXIT_FAILURE);
     }
   }
+
+  for (int i = 0; i < numargs; i++)
+    if (close(outputFD[i]) == -1)
+      exit(EXIT_FAILURE);
 
 }
